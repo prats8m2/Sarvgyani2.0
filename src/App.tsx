d685c62d81ca3ab0dded593baa13a3d9ba5223c7
prateek <prateek3693@gmail.com>
2023-12-22 11:07:22 +0530

    Updates

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Home from './component/Home';
-import './App.scss';
-
-function App() {
-  return <Home title="Sarvagyani" />;
-}
-
-export default App;
--- src/helper/randomInteger.tsx
+++
@@ -1,6 +0,0 @@
-//FUNCTION TO GET RANDOM INTEGER
-const getRndInteger = (min: number, max: number) => {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
-
-export default getRndInteger;
